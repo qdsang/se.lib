cf92fc1ab1878031a5a3929f3448cd0f38385bf8
Lijun <lijun@LijundeMacBook-Air.local>
2014-12-12 10:49:24 +0800

    重新编绎

--- build/js/logic/webapp.a443abe1ff8c7885c576045a65f46331eab5cb13.js
+++ build/js/logic/webapp.a443abe1ff8c7885c576045a65f46331eab5cb13.js
@@ -1,2 +1,2 @@
 /*! Copyright (c) SESHENGHUO.COM - Author: LIJUN - Email: zwlijun@gmail.com - Git: https://github.com/zwlijun/se.lib */
-define(function(require){var a=require("mod/se/webapp.7c831260593d2c9dc8b147746edfa3a20a099abc"),b=$.app=a.newInstance("webapp","view","header","footer");b.set("init",{callback:function(){b.showModuleWidget(0)}}),b.create()});
+define(function(require){var a=require("mod/se/webapp.1ab5c340f4f419133329fbb67f6b9544a2536d70"),b=$.app=a.newInstance("webapp","view","header","footer");b.set("init",{callback:function(){b.showModuleWidget(0)}}),b.create()});
